3822c75fa25e5d541651088a4f21ecb019b4039d
Dustin Fedie <dlfedie@gmail.com>
2020-01-28 18:14:45 -0600

    updates

--- missingIntegerTest/src/com/dust/Main.java
+++ missingIntegerTest/src/com/dust/Main.java
@@ -22,41 +22,48 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        int[] array = new int[3];
 
 
     }
 
+    solution([1,2,3]);
 
-    class Solution {
-        public int solution(int[] A) {
-            //
-            int smallestPositive = 1;
-            Arrays.sort(A);
-            // so if the last # is not positive, just return 1
-            if (A[A.length - 1] < 1) {
-                return smallestPositive;
-            }
-            // create a new array with a value 2 higher than the last value in the initial array, so if #'s are sequential (1,2,3), we'll be able to return 4 (with 5 being the new "last" number)
-            int[] newArray = new int[A.length + 1];
-            for (int i = 0; i < A.length; i++) {
-                newArray[i] = A[i];
-                // add 2, so t
-                newArray[newArray.length - 1] = (A[A.length -1] + 2);
-            }
 
-            // loop through the array now, but we only want to go 1 less than the full length to not get our newly created +2 value
-            for (int j = 0; j < newArray.length - 1; j++) {
-                if (newArray[j +1] - newArray[j] !=1) {
-                    smallestPositive = newArray[j] + 1;
-                    return smallestPositive;
-                } else if (newArray[j + 1] - newArray[j] == 1) {
-                    smallestPositive = newArray[j];
-                }
-            }
-            return smallestPositive + 1;
+    public static void solution(int[] A) {
+        //
+        int smallestPositive = 1;
+        Arrays.sort(A);
+        // so if the last # is not positive, just return 1
+        if (A[A.length - 1] < 1) {
+            System.out.println("lowest int: " + smallestPositive);
+//            return smallestPositive;
+            return;
         }
+        // create a new array with a value 2 higher than the last value in the initial array, so if #'s are sequential (1,2,3), we'll be able to return 4 (with 5 being the new "last" number)
+        int[] newArray = new int[A.length + 1];
+        for (int i = 0; i < A.length; i++) {
+            newArray[i] = A[i];
+            // add 2, so t
+            newArray[newArray.length - 1] = (A[A.length - 1] + 2);
+        }
+
+        // loop through the array now, but we only want to go 1 less than the full length to not get our newly created +2 value
+        for (int j = 0; j < newArray.length - 1; j++) {
+            if (newArray[j + 1] - newArray[j] != 1) {
+                smallestPositive = newArray[j] + 1;
+                System.out.println("lowest int: " + smallestPositive);
+
+                return;
+            } else if (newArray[j + 1] - newArray[j] == 1) {
+                smallestPositive = newArray[j];
+            }
+        }
+        System.out.println("lowest int: " + smallestPositive + 1);
+
+        return;
     }
 
-    
+
 }
